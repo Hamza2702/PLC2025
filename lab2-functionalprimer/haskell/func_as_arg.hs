7dac64acb4315660c430e5be34ebc80d3de8e100
Hamza <114071457+Hamza2702@users.noreply.github.com>
2025-01-29 11:52:51 +0000

    inpFunc with Guards

--- lab2-functionalprimer/haskell/func_as_arg.hs
+++ lab2-functionalprimer/haskell/func_as_arg.hs
@@ -1,14 +1,31 @@
+-- -- create inpFunc
+-- inpFunc a b = [a..b]
+
+-- -- Define applicatorFunc
+-- applicatorFunc inpFunc a b =
+--     fromIntegral (sum (inpFunc a b))
+
+-- main = do
+--     putStrLn "Enter the starting integer (a):"
+--     a <- readLn
+--     putStrLn "Enter the ending integer (b):"
+--     b <- readLn
+--     let result = applicatorFunc inpFunc a b
+--     putStrLn ("Result = " ++ show result)
+
+
+-- // With Guards
+
 -- create inpFunc
 inpFunc a b = [a..b]
 
 -- Define applicatorFunc
-applicatorFunc inpFunc a b =
-    fromIntegral (sum (inpFunc a b))
+applicatorFunc inpFunc a b s | s == 's' = sum (inpFunc a b) | otherwise = (sum (inpFunc a b))/(b - a + 1)
 
 main = do
     putStrLn "Enter the starting integer (a):"
     a <- readLn
     putStrLn "Enter the ending integer (b):"
     b <- readLn
-    let result = applicatorFunc inpFunc a b
+    let result = applicatorFunc inpFunc a b 's'
     putStrLn ("Result = " ++ show result)
